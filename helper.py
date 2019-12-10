b65c9fbb4aa2dca24dd0c142e0b1eff126f8d61b
foncteur <odileradet@live.fr>
2019-12-10 03:19:34 +0100

    Viewing and adding a list seem to work

--- helper.py
+++ helper.py
@@ -9,7 +9,9 @@
 # Import modules
 import sys
 
-# Utils
+'''We assume that the data_lists.txt file already exists'''
+
+## Utils
 def read_file(file):
     '''Returns the list of lines from the file'''
     f = open(file, "r")
@@ -18,7 +20,10 @@ def read_file(file):
     return data
 
 
-# Lists
+## Lists
+'''
+Here, we will find the functions handling the file data_lists.txt.
+'''
 
 '''
 Lists representation :
@@ -39,13 +44,14 @@ def read_lists():
         result.append((line[0], line[1], line[2].split("$")))
     return result
 
-def write_lists(data):
-    f = open("data_lists.txt", "w")
-    for l in data :
-        f.write(l[0] + ";" + l[1] + ";" + "$".join(l[2]) + "\n")
+def write_list(data):
+    '''Writes the new list (stored in data) in the file'''
+    f = open("data_lists.txt", "a")
+    f.write(data[0] + ";" + data[1] + ";" + "$".join(data[2]) + "\n")
     f.close()
 
 def print_list(l):
+    '''Prints a list (pretty-printer)'''
     name = l[0]
     category = l[1]
     content = l[2]
@@ -54,9 +60,10 @@ def print_list(l):
     for item in content :
         print("- {}".format(item))
 
-
-
+## Operators
+# How to ask for a list l
 def view(l):
+    '''Views a list'''
     data = read_lists()
     for list in data :
         if list[0] == l :
@@ -65,6 +72,18 @@ def view(l):
             return 0
     print("Error : list not found")
     return 1
+
+# How to add a list l
+def add(name, cat, list_elts):
+    '''Adds a list'''
+    data = read_lists()
+    for list in data :
+        if list[0] == name :
+            print("Error : a list with this name already exists")
+            return 1
+    write_list([name, cat, list_elts])
+    print("You created the list", name)
+    return 0
 
 
 # Main function
@@ -79,6 +98,18 @@ def main():
             return 1
         else :
             return view(sys.argv[2])
+    elif sys.argv[1] == "add" :
+        if len(sys.argv) <= 2 :
+            print("Error : you need to specify the name of the list you want to add")
+            return 1
+        elif len(sys.argv) <= 3 :
+            print("Error : you need to specify the category of the list you want to add")
+            return 1
+        elif len(sys.argv) <= 4 :
+            print("Error : you are trying to add an empty list")
+            return 1
+        else :
+            return add(sys.argv[2], sys.argv[3], sys.argv[4:])
     else :
         print("Error : unknown command")
         return 1
